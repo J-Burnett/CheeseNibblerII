bb91f0166cc4fbf2b7e8e9cb65a2d93f65b2bb3a
Juli Anna Burnett <julianna.burnett@gmail.com>
2015-02-25 15:55:07 -0700

    Functioning code with comments!

--- CheeseNibblerII/CheeseNibblerII/Program.cs
+++ CheeseNibblerII/CheeseNibblerII/Program.cs
@@ -33,6 +33,7 @@ namespace CheeseNibblerII
         public int Y { get; set; }
         public PointStatus Status { get; set; }
 
+        //Point constructor
         public Point(int x, int y)
         {
             this.X = x;
@@ -42,43 +43,62 @@ namespace CheeseNibblerII
 
     }
 
+    /// <summary>
+    /// Class containing Mouse and its properties
+    /// </summary>
     public class Mouse
     {
+        //Mouse Properties
         public Point Position { get; set; }
         public int Energy { get; set; }
         public bool HasBeenPouncedOn { get; set; }
 
+        //Mouse constructor
         public Mouse()
         {
+            //starting energy
             this.Energy = 50;
+            //end game condition
             this.HasBeenPouncedOn = false;
         }
     }
 
+    /// <summary>
+    /// Class containing Cat and its properties
+    /// </summary>
     public class Cat
     {
+        //Point property
         public Point Position { get; set; }
 
+        //Cat constructor with no additional conditions
         public Cat()
         {
         
         }
     }
 
+    /// <summary>
+    /// Main game logic
+    /// </summary>
     public class CheeseNibbler
     {
-        public Random rng { get; set; }
+        //All necessary properties to run game
+        public Random rng { get; set; }//random number generator
         public Point[,] Grid { get; set; }//Grid itself
         public Mouse Mouse { get; set; }//Our Mouse
         public Point Cheese { get; set; }//Sought after cheese
         public int CheeseCount { get; set; }//Pieces of cheese nibbled  
         public List<Cat> Cats { get; set; }//List of Cats to chase Mouse
-        public bool MoveIsCat = false;
 
+        /// <summary>
+        /// Main game constructor
+        /// </summary>
         public CheeseNibbler()
         {
+            //initializes mouse
             this.Mouse = new Mouse();
-            
+            //initializes random number
             this.rng = new Random(); 
 
             //initialize the grid
@@ -101,7 +121,7 @@ namespace CheeseNibblerII
             //Places the cheese
             PlaceCheese();
            
-            // place a mouse
+            //Place a mouse
             do
             {
                 this.Mouse.Position = this.Grid[rng.Next(this.Grid.GetLength(0)), rng.Next(this.Grid.GetLength(1))];
@@ -109,16 +129,20 @@ namespace CheeseNibblerII
             this.Mouse.Position.Status = Point.PointStatus.Mouse;
         }
 
+        /// <summary>
+        /// Creates grid to play game
+        /// </summary>
         public void DrawGrid()
         {
             //Clear the console
             Console.Clear();
 
+            //Populates grid with various point types
             for(int y = 0; y < this.Grid.GetLength(1); y++)
             {
                 for(int x = 0; x < this.Grid.GetLength(0); x++)
                 {
-                    //check the type of each point
+                    //check and prints type of each point
                     switch (Grid[x, y].Status)
                     {
                         case Point.PointStatus.Mouse:
@@ -138,20 +162,28 @@ namespace CheeseNibblerII
                             break;
                     }
                 }
+                //Adds line for each row
                 Console.WriteLine();
             }
+            //Prints game play messages
             Console.WriteLine("Use the numpad to move the mouse!");
             Console.WriteLine("\nPieces of Cheese: {0}", CheeseCount);
             Console.WriteLine("Mouse Energy: {0}", Mouse.Energy);
         }
 
+        /// <summary>
+        /// Takes in player move
+        /// </summary>
+        /// <returns>The player input</returns>
         public ConsoleKey GetUserMove()
         {
-            //putting a single key stroke into a variable
-            //(true) keeps the character from being written to the console
+            //gets players key stroke
             ConsoleKey playerInput = Console.ReadKey(true).Key;
 
+            //Condition     
             bool validInput = false;
+
+            //loops through possible keystrokes
             while (!validInput)
             {
                 //checks for valid input
@@ -168,16 +200,23 @@ namespace CheeseNibblerII
                         validInput = true;
                         return playerInput;
                     default:
-                        //invalid
+                        //Prints error messsage
                         Console.WriteLine("\nNot a valid move");
+                        //reminds player which keys to press
                         Console.WriteLine("Use the numpad to move the mouse");
                         playerInput = Console.ReadKey(true).Key;
                         return playerInput;
                 }
             }
+            //returns the input 
             return playerInput;
         }
 
+        /// <summary>
+        /// Checks if the players input was a valid keystroke
+        /// </summary>
+        /// <param name="playerInput">Keystroke of player</param>
+        /// <returns>Whether or not it was valid</returns>
         public bool ValidMove(ConsoleKey playerInput)
         {
             //look at each type of keypress
@@ -206,13 +245,20 @@ namespace CheeseNibblerII
             return false;
         }
 
+        /// <summary>
+        /// Game logic to move the mouse based on player's input
+        /// </summary>
+        /// <param name="playerInput">Keystroke used to move mouse</param>
+        /// <returns></returns>
         public bool MoveMouse(ConsoleKey playerInput)
         {
+            //creates new positions for the mouse
             int nextX = this.Mouse.Position.X;
             int nextY = this.Mouse.Position.Y;
-
+            //Checks if move was valid
             if (ValidMove(playerInput))
             {
+                //if so, moves the mouse accordingly
                 switch (playerInput)
                 {
                     case ConsoleKey.NumPad4://left
@@ -246,67 +292,93 @@ namespace CheeseNibblerII
                     default:
                         break;
                 }
+                //decrements mouse 
+                //energy after each move
                 Mouse.Energy--;
             }
-
+            //sets mouse's next move on the grid
             Point nextMouseMove = this.Grid[nextX, nextY];
 
+            //checks if space has the cheese
             if (nextMouseMove.Status == Point.PointStatus.Cheese)
             {
+                //if so, empty old mouse point
                 this.Grid[Mouse.Position.X, Mouse.Position.Y].Status = Point.PointStatus.Empty;
+                //updates to new mouse point
                 this.Grid[nextX, nextY].Status = Point.PointStatus.Mouse;
                 this.Mouse.Position = this.Grid[nextX, nextY];
                 return true;
             }
+                //if the next move is on the same point as the cat or the cat&cheese
             else if (nextMouseMove.Status == Point.PointStatus.Cat || nextMouseMove.Status == Point.PointStatus.CatAndCheese)
             {
+                //sets mouse has been pounced on to true
                 Mouse.HasBeenPouncedOn = true;
-                // mabye move mouse here
                 return false;
             }
+                //otherwise,
             else
             {
+                //empty old position
                 this.Grid[Mouse.Position.X, Mouse.Position.Y].Status = Point.PointStatus.Empty;
+                //set mouse to the new position
                 this.Grid[nextX, nextY].Status = Point.PointStatus.Mouse;
                 this.Mouse.Position = this.Grid[nextX, nextY];
                 return false;
             }
         }
 
+        /// <summary>
+        /// Places the cheese on a random point on the grid
+        /// </summary>
         public void PlaceCheese()
         {
+            //randomize the points for the cheese on the grid
             do
             {
-
                 this.Cheese = Grid[rng.Next(0, 10), rng.Next(0, 10)];
-
             } while (this.Cheese.Status != Point.PointStatus.Empty);
 
             //adds cheese to grid with random coordinates
             this.Cheese.Status = Point.PointStatus.Cheese;
         }
 
+        /// <summary>
+        /// Adds a new cat to the Cats list
+        /// </summary>
         public void AddCat()
         {
+            //create the cat
             Cat cat = new Cat();
-            PlaceCat(cat);
-            this.Cats.Add(cat);
+            PlaceCat(cat);//calls PlaceCat with new cat
+            this.Cats.Add(cat);//adds new cat to the Cats list
         }
 
+        /// <summary>
+        /// Places the cat on the grid
+        /// </summary>
+        /// <param name="cat">Cat from the cat list</param>
         public void PlaceCat(Cat cat)
         {
-
+            //randomly picks points on the grid to place cat
             do{
                 cat.Position = this.Grid[rng.Next(this.Grid.GetLength(0)), rng.Next(this.Grid.GetLength(1))];
                 
             } while(cat.Position.Status != Point.PointStatus.Empty);
+            //sets cat position to the random coordinates
             cat.Position.Status = Point.PointStatus.Cat;
         }
 
+        /// <summary>
+        /// automatically moves the cat on the grid
+        /// </summary>
+        /// <param name="cat">Cat to be moved</param>
         public void MoveCat(Cat cat)
         {
+            //80% chance of cat moving
             if (rng.Next(0, 6) < 4)
             {
+                //booleans for moving cat
                 int xDiff = Mouse.Position.X - cat.Position.X;
                 int yDiff = Mouse.Position.Y - cat.Position.Y;
                 bool tryLeft = xDiff < 0;
@@ -314,34 +386,37 @@ namespace CheeseNibblerII
                 bool tryUp = yDiff < 0;
                 bool tryDown = yDiff > 0;
                 bool validMove = false;
-
+                //cats future position on grid
                 Point targetPosition = cat.Position;
 
+                //checks if move is valid
                 while (!validMove && (tryLeft || tryRight || tryUp || tryDown))
                 {
+                    //new cat coordinates
                     int catX = cat.Position.X;
                     int catY = cat.Position.Y;
-
-                    if (tryLeft)
+                    
+                    if (tryLeft)//if room to move left
                     {
-                        targetPosition = Grid[--catX, catY];
+                        targetPosition = Grid[--catX, catY];//moves cat left
                         tryLeft = false;
                     }
-                    else if (tryRight)
+                    else if (tryRight)//if room to move right
                     {
-                        targetPosition = Grid[++catX, catY];
+                        targetPosition = Grid[++catX, catY];//moves cat right
                         tryRight = false;
                     }
-                    else if (tryUp)
+                    else if (tryUp)//if room to move up
                     {
-                        targetPosition = Grid[catX, --catY];
+                        targetPosition = Grid[catX, --catY];//moves cat up
                         tryUp = false;
                     }
-                    else if (tryDown)
+                    else if (tryDown)//if room to move down
                     {
-                        targetPosition = Grid[catX, ++catY];
+                        targetPosition = Grid[catX, ++catY];//moves cat down
                         tryDown = false;
                     }
+                    //calls valid cat function with targetposition as parameter
                     validMove = ValidCatMoveOnBoard(targetPosition);
 
                 }
@@ -370,21 +445,40 @@ namespace CheeseNibblerII
             }
 
         }
-
+        
+        /// <summary>
+        /// Checks if the cat's move is valid
+        /// </summary>
+        /// <param name="targetLocation">Point on the grid the cat is moving towards</param>
+        /// <returns>True or false</returns>
         public bool ValidCatMoveOnBoard(Point targetLocation)
         {
+            //checks if the future cat move is to any spot other than one already occupied by a cat
             return targetLocation.Status == Point.PointStatus.Empty || targetLocation.Status == Point.PointStatus.Mouse || targetLocation.Status == Point.PointStatus.Cheese;
         }
 
+        /// <summary>
+        /// Automatically moves the cat on the board
+        /// </summary>
+        /// <param name="targetPosition">Position to move the cat</param>
+        /// <returns>true or false</returns>
         public bool CatsTurnToMove(Point targetPosition)
         {
+            //checks position if the position has a cat
             return targetPosition.Status == Point.PointStatus.Cat || targetPosition.Status == Point.PointStatus.CatAndCheese;
         }
 
+        /// <summary>
+        /// Plays the game 
+        /// </summary>
         public void PlayGame()
         {
+            //boolean finding cheese
             bool hasFoundCheese = false;
+            //cheese counter
             this.CheeseCount = 0;
+
+            //game play condition
             while(Mouse.Energy > 0)
             {
                 //Draw the grid
@@ -397,23 +491,32 @@ namespace CheeseNibblerII
                 //if the game has ended
                 hasFoundCheese = MoveMouse(playerMove);
 
+                //if the mouse has been caught
                 if (this.Mouse.HasBeenPouncedOn)
                 {
+                    //kill the mouse
                     this.Mouse.Energy = 0;
                 }
 
+                //loops through the cats in cat list
                 foreach(Cat cat in Cats)
                 {
+                    //run each cat through move cat list
                     this.MoveCat(cat);
                 }
-
+                //if cheese was found
                 if (hasFoundCheese)
                 {
+                    //replaces the cheese
                     PlaceCheese();
+                    //adds to the mouse's energy
                     this.Mouse.Energy += 10;
+                    //add to the cheese counter
                     CheeseCount++;
+                    //for every two pieces of cheese eaten
                     if (CheeseCount % 2 == 0)
                     {
+                        //add a cat to the grid
                         AddCat();
                     }
                 }
